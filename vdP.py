bd02723fa80aa488437054c921c71d8887c6bd6a
Hezy Amiel <hezy.amiel@gmail.com>
2023-01-31 20:03:26 +0200

    /

--- vdP.py
+++ vdP.py
@@ -11,7 +11,7 @@ from scipy import optimize
 
 """
 find the sheet resistance Rs from R1 = R_AB,CD and R2 = R_BC,AD
-the solution is from the equation in a paper L. J. van der Pauw
+the solution is from the equation in a paper by L. J. van der Pauw:
 "A method of measuring the resistivity and hall coefficients of
 Lamellae of arbitrary shape", Philips Technical Review, Vol 26, 220.
 """
